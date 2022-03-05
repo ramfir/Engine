7d12adb5c48a45be621377d74f2c66d1a1685333
Firdavs <firdavs_20111@mail.ru>
2022-03-05 19:28:03 +0300

    refactor MainActivity and activity_main.xml

--- app/src/main/java/com/example/engine/MainActivity.kt
+++ app/src/main/java/com/example/engine/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.engine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.engine.databinding.ActivityMainBinding
+import com.example.engine.model.Person
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val personAdapter = PersonAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupRecyclerView()
+        fillData()
+    }
+
+    private fun fillData() {
+        personAdapter.submitList(Person.persons)
+    }
+
+    private fun setupRecyclerView() {
+        with(binding) {
+            recyclerViewPersons.adapter = personAdapter
+        }
     }
 }
