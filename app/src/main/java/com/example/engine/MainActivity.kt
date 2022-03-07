ab6bed170891c117ac60821c1aaeab8ff222766d
Firdavs <firdavs_20111@mail.ru>
2022-03-07 11:37:14 +0300

    implemented search by surname

--- app/src/main/java/com/example/engine/MainActivity.kt
+++ app/src/main/java/com/example/engine/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.engine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.engine.databinding.ActivityMainBinding
 import com.example.engine.model.Person
@@ -20,15 +24,28 @@ class MainActivity : AppCompatActivity() {
 
         setupRecyclerView()
         fillData()
+        setupSearchProcess()
+    }
+
+    private fun setupRecyclerView() {
+        with(binding) {
+            recyclerViewPersons.apply {
+                adapter = personAdapter
+                addItemDecoration(
+                    DividerItemDecoration(this@MainActivity, LinearLayoutManager.VERTICAL))
+            }
+        }
     }
 
     private fun fillData() {
         personAdapter.submitList(Person.persons)
     }
 
-    private fun setupRecyclerView() {
-        with(binding) {
-            recyclerViewPersons.adapter = personAdapter
+    private fun setupSearchProcess() {
+        binding.editTextSearchSurname.addTextChangedListener { surname ->
+            val filteredList = Person.persons.filter { person ->
+                person.surname.contains(surname.toString(), true)}
+            personAdapter.submitList(filteredList)
         }
     }
 }
